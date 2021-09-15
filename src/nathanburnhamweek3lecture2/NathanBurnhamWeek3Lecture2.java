12334a7610a79e888991cd95a1fc4f21321bc3f3
soblab <soblab@WHIT208W10_07.it.farmingdale.edu>
2021-09-15 10:19:23 -0400

    this is a test

---
+++ src/nathanburnhamweek3lecture2/NathanBurnhamWeek3Lecture2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package nathanburnhamweek3lecture2;
+
+/**
+ *
+ * @author soblab
+ */
+/*public class NathanBurnhamWeek3Lecture2 {
+
+   public static int getSum(int num1, int num2)
+   {
+       int total;
+       total = num1+num2;
+       return total;
+   }
+   
+   public static void main(String[] args)
+   {
+       int x = 10;
+       int y = 20;
+       int sum;
+       sum = getSum(x, y);
+       System.out.println("Sum is: " + sum);
+   }
+    
+}*/
+
+
